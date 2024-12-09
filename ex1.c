23ebde92dfa29ea6113a096da23af66ea1174853
Rodolfo Cabral Neves <roddas360@gmail.com>
2024-12-09 08:26:28 -0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -5,15 +5,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-void solicitacao_dados(void);
+void solicitacao_integer(void);
+void solicitacao_float(void);
 
-int main()
-{
-	solicitacao_dados();
-    return 0;
+int main() {
+	solicitacao_integer();
+	solicitacao_float();
+	return 0;
 }
 
-void solicitacao_dados(void){
+void solicitacao_float(void){
+	int uart0_filestream = -1;
+
+    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY); // Open in non blocking read/write mode
+    if (uart0_filestream == -1)
+    {
+        printf("Erro - Não foi possível iniciar a UART.\n");
+    }
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD; //<Set baud rate
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    unsigned char tx_buffer[255];
+    unsigned char *p_tx_buffer;
+
+    p_tx_buffer = &tx_buffer[0];
+
+    *p_tx_buffer++ = 0xa2;
+    *p_tx_buffer++ = 1; // 1 
+    *p_tx_buffer++ = 4; // 4
+    *p_tx_buffer++ = 7; // 7
+    *p_tx_buffer++ = 2; // 2
+
+    if (uart0_filestream != -1)
+    {
+        int count = write(uart0_filestream, &tx_buffer, (p_tx_buffer - &tx_buffer[0]));
+        if (count <= 0)
+        {
+            printf("UART TX error\n");
+        }
+    }
+
+    sleep(1);
+
+    //----- CHECK FOR ANY RX BYTES -----
+    if (uart0_filestream != -1)
+    {
+        // Read up to 255 characters from the port if they are there
+	float rx_buffer;
+        int rx_length = read(uart0_filestream, &rx_buffer, 50); // Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            printf("Erro na leitura.\n"); // An error occured (will occur if there are no bytes)
+        }
+        else if (rx_length == 0)
+        {
+            printf("Nenhum dado disponível.\n"); // No data waiting
+        }
+        else
+        {
+            // Bytes received
+            printf("Solicita FLOAT\t(0xA2) - Retorno = %f\n", rx_buffer);
+        }
+    }
+
+    close(uart0_filestream);
+}
+
+
+void solicitacao_integer(void){
 	    int uart0_filestream = -1;
 
     uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY); // Open in non blocking read/write mode
@@ -57,7 +122,7 @@ void solicitacao_dados(void){
     {
         // Read up to 255 characters from the port if they are there
         int rx_buffer;
-        int rx_length = read(uart0_filestream, &rx_buffer, 5); // Filestream, buffer to store in, number of bytes to read (max)
+        int rx_length = read(uart0_filestream, &rx_buffer, 50); // Filestream, buffer to store in, number of bytes to read (max)
         if (rx_length < 0)
         {
             printf("Erro na leitura.\n"); // An error occured (will occur if there are no bytes)
@@ -75,6 +140,8 @@ void solicitacao_dados(void){
 
     close(uart0_filestream);
 }
+
+
 
 /*
 ===============
